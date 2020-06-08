032cfc7141ce965b1c7c7e57920f32e68d37a253
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-08 14:29:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.take(-1)
+  array.take(4)
 end
 
 def retrieve_first_element_from_array(array)
