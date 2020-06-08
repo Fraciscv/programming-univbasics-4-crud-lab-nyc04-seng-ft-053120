2d8a7ee308b0f3905b04fdcbcb11a882bd2987a6
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-08 14:14:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,8 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array = ["1", "2", "3", "4"]
+  #create_an_array = ["1", "2", "3", "4"]
+  create_an_array.new.length(4)
 end
 
 def add_element_to_end_of_array(array, element)
